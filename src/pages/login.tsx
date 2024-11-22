76e60f2a2c78dc76c1cca822db746b9d42fa8bea
N-eeraj <neeraj7rajeena@gmail.com>
2024-11-22 14:18:04 +0530

    Login Form Landscape UI

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -23,21 +23,21 @@ function Login() {
   }
 
   return (
-    <main className="grid justify-items-center items-center min-h-screen max-lg:py-11 bg-black text-primary-text">
-      <article className="flex justify-between w-full max-w-3xl max-lg:h-full">
+    <main className="grid justify-items-center items-center lg:items-start min-h-screen max-lg:py-11 bg-black text-primary-text">
+      <article className="flex justify-between lg:justify-center lg:gap-x-8 w-full max-w-3xl max-lg:h-full lg:mt-16">
         <img
           src="/images/login/screen.png"
           alt="instagram-screens"
-          className="max-lg:hidden h-[80vh]" />
+          className="max-lg:hidden w-96" />
 
-        <section className="flex flex-col justify-between items-center w-full max-w-72 mx-auto">
-          <div>
+        <section className="flex flex-col justify-between lg:justify-start items-center lg:gap-y-2.5 w-full lg:w-80 mx-auto lg:mx-0">
+          <div className="flex flex-col items-center lg:w-full lg:pb-7 lg:border lg:border-separator-dark">
             <Icon
               icon="logos:instagram"
-              className="mx-auto text-5xl brightness-0 invert" />
+              className="mx-auto lg:mt-9 text-5xl brightness-0 invert" />
 
             <form
-              className="flex flex-col gap-y-2 w-72 pt-7"
+              className="flex flex-col gap-y-2 w-72 lg:w-full pt-7 lg:px-3"
               onSubmit={handleLoginSubmit}>
               <Input
                 placeholder="Email address"
@@ -52,7 +52,7 @@ function Login() {
             </form>
           </div>
 
-          <div className="pb-2.5 text-sm">
+          <div className="lg:flex lg:justify-center lg:items-center lg:w-full lg:h-16 text-sm lg:border lg:border-separator-light">
             <span className="mr-1">
               Don't have an account?
             </span>
